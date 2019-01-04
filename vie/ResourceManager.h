0ed522273f53c6803faca2c84cb0d261d587d0bb
Ajver <zawlockidominik@gmail.com>
2019-01-04 23:13:32 +0100

    ...

--- vie/ResourceManager.h
+++ vie/ResourceManager.h
@@ -16,8 +16,8 @@ namespace vie
 	private:
 		static std::map<std::string, Texture> texturesMap;
 
+		static bool readFileToBuffer(std::string filePath, std::vector<unsigned char>& buffer);
 		static Texture loadPNG(std::string filePath); 
-		static bool readFileToBuffer(std::string, std::vector<unsigned char> &);
 		static int decodePNG(std::vector<unsigned char>&, unsigned long&, unsigned long&, const unsigned char*, size_t, bool = true);
 	};
 
